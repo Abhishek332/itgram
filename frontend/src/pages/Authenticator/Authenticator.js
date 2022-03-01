e227939df87f8290e7b937cb8057e5a5d3f9ee1a
Abhishek332 <itsabhiofficial.21@gmail.com>
2022-03-01 22:20:48 +0530

    Working progress in SignUp/SignIn

--- frontend/src/pages/Authenticator/Authenticator.js
+++ frontend/src/pages/Authenticator/Authenticator.js
@@ -4,21 +4,69 @@ import { useState } from "react";
 import { useLocation } from "react-router-dom";
 
 const Authenticator = () => {
-  const { search } = useLocation();
-  const [showSignUp, setShowSignUp] = useState(search.includes("signup"));
-  console.log(showSignUp);
+  const { search } = useLocation(),
+    [showSignUp, setShowSignUp] = useState(search.includes("signup")),
+    [state, setState] = useState(
+      showSignUp
+        ? {
+            name: "",
+            email: "",
+            password: "",
+          }
+        : {
+            email: "",
+            password: "",
+          }
+    );
+
+  const handleChange = ({ target: { name, value } }) => {
+    setState({ ...state, [name]: value });
+  };
+
+  const handleSubmit = () => {
+    console.log(state);
+  };
   return (
     <>
       <div className="authenticator-container">
         <div className="auth-box">
           <div className="form-box">
-            <form>
-              <label style={{ display: "flex", flexDirection: "column" }}>
-                Name:
-                <input type="text" name="name" />
-              </label>
-              <input type="submit" value="Submit" />
-            </form>
+            <h2>{showSignUp ? "SignUp" : "SignIn"}</h2>
+            {showSignUp && (
+              <input
+                type="text"
+                name="name"
+                placeholder="Enter your name*"
+                value={state.name}
+                onChange={handleChange}
+                required
+              />
+            )}
+            <input
+              type="email"
+              name="email"
+              placeholder="Enter your email*"
+              value={state.email}
+              onChange={handleChange}
+              required
+            />
+            <input
+              type="password"
+              name="password"
+              placeholder="Password*"
+              value={state.password}
+              onChange={handleChange}
+              required
+            />
+            <button onClick={handleSubmit}>Submit</button>
+            <p>
+              {showSignUp
+                ? "Already have an account?"
+                : "Don't have an account?"}
+              <span onClick={() => setShowSignUp(!showSignUp)}>
+                {showSignUp ? "SignIn" : "SignUp"}
+              </span>
+            </p>
           </div>
           <img src={WelCome} alt="" />
         </div>
