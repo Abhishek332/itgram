d6c46ffa535df343315508faa299f224d650768f
Abhishek332 <itsabhiofficial.21@gmail.com>
2022-03-03 00:53:48 +0530

    Add HomePage

---
+++ frontend/src/pages/HomePage/HomePage.js
@@ -0,0 +1,13 @@
+import { NavBar } from "../../components";
+import "./HomePage.scss";
+
+const HomePage = () => {
+  return (
+    <>
+      <NavBar />
+      <div className="homepage-container">HomePage</div>
+    </>
+  );
+};
+
+export default HomePage;
