a73bc105470a0b34b829e6ea75e77af6f672adc9
Abhishek332 <itsabhiofficial.21@gmail.com>
2022-03-03 22:44:10 +0530

    Post Card Complete

--- frontend/src/assets/images/index.js
+++ frontend/src/assets/images/index.js
@@ -3,5 +3,6 @@ import Loader2 from "./loader2.gif";
 import Loader3 from "./loader3.gif";
 import WelCome from "./welcome.gif";
 import Logo from "./logo.png";
+import Like from "./like.gif";
 
-export { Loader1, Loader2, Loader3, WelCome, Logo };
+export { Loader1, Loader2, Loader3, WelCome, Logo, Like };
