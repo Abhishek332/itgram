141030e39e63b2abb726601086654f440e3c66aa
abhishek332 <itsabhiofficial.21@gmail.com>
2022-03-04 00:57:24 +0530

    small changes

--- frontend/src/pages/CreatePost/CreatePost.js
+++ frontend/src/pages/CreatePost/CreatePost.js
@@ -1,7 +1,58 @@
 import './CreatePost.scss';
+import { useState } from 'react';
+import { Loader3 } from '../../assets/images';
+import { NavBar } from '../../components';
 
 const CreatePost = () => {
-	return <div>CreatePost</div>;
+	const [state, setState] = useState({
+		title: '',
+		body: '',
+	});
+	const [loader, setLoader] = useState(false);
+
+	const handleChange = ({ target: { name, value } }) => {
+		setState({ ...state, [name]: value });
+		console.log('state', state);
+	};
+
+	const handleSubmit = () => {
+		console.log(state);
+		setLoader(true);
+		setTimeout(() => {
+			setLoader(false);
+		}, 1000);
+	};
+
+	return (
+		<>
+			<NavBar />
+			<div className='create-post-container'>
+				<div className='form-wrapper'>
+					<input
+						type='text'
+						name='title'
+						placeholder='Title'
+						value={state.title}
+						onChange={handleChange}
+					/>
+					<textarea
+						name='body'
+						placeholder='Enter post description'
+						value={state.body}
+						onChange={handleChange}
+					/>
+					<button className='post-btn' onClick={handleSubmit}>
+						Post
+					</button>
+				</div>
+				{loader && (
+					<div className='loader'>
+						<img src={Loader3} alt='' />
+					</div>
+				)}
+			</div>
+		</>
+	);
 };
 
 export default CreatePost;
