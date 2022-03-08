7881cdd603c5529851d22554bedc72eae4ebac57
Abhishek332 <itsabhiofficial.21@gmail.com>
2022-03-08 14:37:08 +0530

    Calling Api and Response set for Login / SignUp

--- frontend/src/pages/Authenticator/Authenticator.js
+++ frontend/src/pages/Authenticator/Authenticator.js
@@ -1,97 +1,117 @@
-import './Authenticator.scss';
-import { WelCome, Lock } from '../../assets/images';
-import { useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-import { userSignUp } from '../../redux/actions/authActions';
+import "./Authenticator.scss";
+import { WelCome, Lock } from "../../assets/images";
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { userSignUp, userLogIn } from "../../redux/actions/authActions";
 
 const Authenticator = () => {
-	const { search } = useLocation(),
-		dispatch = useDispatch(),
-		{
-			loading: UserSignUpLoading,
-			userInfo: UserSignUpInfo,
-			error: UserSignUpError,
-		} = useSelector((state) => state.userSignUp),
-		[showSignUp, setShowSignUp] = useState(search.includes('signup')),
-		[state, setState] = useState(
-			showSignUp
-				? {
-						name: '',
-						email: '',
-						password: '',
-				  }
-				: {
-						email: '',
-						password: '',
-				  }
-		);
+  const { search } = useLocation(),
+    dispatch = useDispatch(),
+    {
+      loading: UserSignUpLoading,
+      userInfo: UserSignUpInfo,
+      error: UserSignUpError,
+    } = useSelector((state) => state.userSignUp),
+    {
+      loading: UserLogInLoading,
+      userInfo: UserLogInInfo,
+      error: UserLogInError,
+    } = useSelector((state) => state.userLogIn),
+    [showSignUp, setShowSignUp] = useState(search.includes("signup")),
+    [state, setState] = useState(
+      showSignUp
+        ? {
+            name: "",
+            email: "",
+            password: "",
+          }
+        : {
+            email: "",
+            password: "",
+          }
+    );
 
-	useEffect(() => {
-		console.log('UserSignUpLoading', UserSignUpLoading);
-		console.log('UserSignUpInfo', UserSignUpInfo);
-		console.log('UserSignUpError', UserSignUpError);
-	}, [UserSignUpLoading, UserSignUpInfo, UserSignUpError]);
+  useEffect(() => {
+    if (showSignUp) {
+      console.log("UserSignUpLoading", UserSignUpLoading);
+      console.log("UserSignUpInfo", UserSignUpInfo);
+      console.log("UserSignUpError", UserSignUpError);
+      return;
+    }
+    console.log("UserLogInLoading", UserLogInLoading);
+    console.log("UserLogInInfo", UserLogInInfo);
+    console.log("UserLogInError", UserLogInError);
+  }, [
+    UserSignUpLoading,
+    UserSignUpInfo,
+    UserSignUpError,
+    UserLogInLoading,
+    UserLogInInfo,
+    UserLogInError,
+    showSignUp,
+  ]);
 
-	const handleChange = ({ target: { name, value } }) => {
-			setState({ ...state, [name]: value });
-		},
-		handleSubmit = () => {
-			dispatch(userSignUp(state));
-		};
+  const handleChange = ({ target: { name, value } }) => {
+      setState({ ...state, [name]: value });
+    },
+    handleSubmit = () => {
+      console.log("state", state);
+      showSignUp ? dispatch(userSignUp(state)) : dispatch(userLogIn(state));
+    };
 
-	return (
-		<>
-			<div className='authenticator-container'>
-				<div className='auth-box'>
-					<div className='form-box'>
-						<h2>{showSignUp ? 'SignUp' : 'SignIn'}</h2>
-						{showSignUp && (
-							<input
-								type='text'
-								name='name'
-								placeholder='Enter your name*'
-								value={state.name}
-								onChange={handleChange}
-								required
-							/>
-						)}
-						<input
-							type='email'
-							name='email'
-							placeholder='Enter your email*'
-							value={state.email}
-							onChange={handleChange}
-							required
-						/>
-						<input
-							type='password'
-							name='password'
-							placeholder='Password*'
-							value={state.password}
-							onChange={handleChange}
-							required
-						/>
-						<button onClick={handleSubmit}>Submit</button>
-						<p>
-							{showSignUp
-								? 'Already have an account?'
-								: "Don't have an account?"}
-							<span onClick={() => setShowSignUp(!showSignUp)}>
-								{showSignUp ? ' SignIn' : ' SignUp'}
-							</span>
-						</p>
-					</div>
-					<img src={WelCome} alt='' />
-				</div>
-			</div>
-			{UserSignUpLoading && (
-				<div className='lock-loader'>
-					<img src={Lock} alt='' />
-				</div>
-			)}
-		</>
-	);
+  return (
+    <>
+      <div className="authenticator-container">
+        <div className="auth-box">
+          <div className="form-box">
+            <h2>{showSignUp ? "SignUp" : "SignIn"}</h2>
+            {showSignUp && (
+              <input
+                type="text"
+                name="name"
+                placeholder="Enter your name*"
+                value={state.name}
+                onChange={handleChange}
+                required
+              />
+            )}
+            <input
+              type="email"
+              name="email"
+              placeholder="Enter your email*"
+              value={state.email}
+              onChange={handleChange}
+              required
+            />
+            <input
+              type="password"
+              name="password"
+              placeholder="Password*"
+              value={state.password}
+              onChange={handleChange}
+              required
+            />
+            <button onClick={handleSubmit}>Submit</button>
+            <p>
+              {showSignUp
+                ? "Already have an account?"
+                : "Don't have an account?"}
+              <span onClick={() => setShowSignUp(!showSignUp)}>
+                {showSignUp ? " SignIn" : " SignUp"}
+              </span>
+            </p>
+          </div>
+          <img src={WelCome} alt="" />
+        </div>
+      </div>
+      {(UserSignUpLoading || UserLogInLoading) && (
+        <div className="lock-loader">
+          <img src={Lock} alt="" />
+        </div>
+      )}
+    </>
+  );
 };
 
 export default Authenticator;
--- frontend/src/redux/actions/authActions.js
+++ frontend/src/redux/actions/authActions.js
@@ -1,27 +1,54 @@
-import { USER_SIGNUP } from '../constants/authConstants';
-import { axios } from '../../api/axios';
+import { USER_SIGNUP, USER_LOGIN } from "../constants/authConstants";
+import { axios } from "../../api/axios";
 
-export const userSignUp = (obj = { name: '', email: '', password: '' }) => {
-	return async (dispatch) => {
-		dispatch({ type: USER_SIGNUP.REQUEST });
+export const userSignUp = (obj = { name: "", email: "", password: "" }) => {
+  return async (dispatch) => {
+    dispatch({ type: USER_SIGNUP.REQUEST });
 
-		try {
-			axios
-				.post('/signup', obj)
-				.then((res) => {
-					dispatch({ type: USER_SIGNUP.SUCCESS, payload: res.data });
-				})
-				.catch((err) => {
-					dispatch({
-						type: USER_SIGNUP.FAIL,
-						payload: err.response.data.error,
-					});
-				});
-		} catch (error) {
-			dispatch({
-				type: USER_SIGNUP.FAIL,
-				payload: 'Something Went Wrong',
-			});
-		}
-	};
+    try {
+      axios
+        .post("/signup", obj)
+        .then((res) => {
+          dispatch({ type: USER_SIGNUP.SUCCESS, payload: res.data });
+        })
+        .catch((err) => {
+          dispatch({
+            type: USER_SIGNUP.FAIL,
+            payload: err.response.data.error,
+          });
+        });
+    } catch (error) {
+      console.log(error);
+      dispatch({
+        type: USER_SIGNUP.FAIL,
+        payload: "Something Went Wrong, Please try again",
+      });
+    }
+  };
+};
+
+export const userLogIn = (obj = { email: "", password: "" }) => {
+  return async (dispatch) => {
+    dispatch({ type: USER_LOGIN.REQUEST });
+
+    try {
+      axios
+        .post("/signin", obj)
+        .then((res) => {
+          dispatch({ type: USER_LOGIN.SUCCESS, payload: res.data });
+        })
+        .catch((err) => {
+          dispatch({
+            type: USER_LOGIN.FAIL,
+            payload: err.response.data.error,
+          });
+        });
+    } catch (error) {
+      console.log(error);
+      dispatch({
+        type: USER_LOGIN.FAIL,
+        payload: "Something Went Wrong, Please try again",
+      });
+    }
+  };
 };
