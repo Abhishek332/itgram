7f33f3fba554912cff46b4a693aa77895371a6c6
Abhishek332 <itsabhiofficial.21@gmail.com>
2022-03-16 00:51:26 +0530

    remove unused code

--- frontend/src/pages/HomePage/HomePage.js
+++ frontend/src/pages/HomePage/HomePage.js
@@ -5,13 +5,11 @@ import "./HomePage.scss";
 import { allPostCall } from "../../redux/actions/postActions";
 import { useDispatch, useSelector } from "react-redux";
 import { Loader2 } from "../../assets/images";
-import { useToaster, ToastBox } from "../../utils/toaster";
 
 const HomePage = () => {
   const navigate = useNavigate(),
     dispatch = useDispatch(),
-    toaster = useToaster(),
-    { loading, allpost, error } = useSelector((state) => state.allPost);
+    { loading, allpost } = useSelector((state) => state.allPost);
 
   useEffect(() => {
     if (!localStorage.getItem("userInfo")) navigate("/authenticator");
@@ -20,11 +18,6 @@ const HomePage = () => {
   useEffect(() => {
     dispatch(allPostCall());
   }, [dispatch]);
-
-  useEffect(() => {
-    if (!error) return;
-    toaster("error", error);
-  }, [error, toaster]);
 
   console.log("allpost", allpost);
 
@@ -39,7 +32,6 @@ const HomePage = () => {
       </div>
       <Footer />
       {loading && <Loader Illustration={Loader2} />}
-      <ToastBox />
     </>
   );
 };
