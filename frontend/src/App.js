cca9e87764700ff1506f750ecaa9215f52402a1d
Abhishek332 <itsabhiofficial.21@gmail.com>
2022-03-23 22:17:24 +0530

    Add UserProfile Route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,22 +6,22 @@ import {
   HomePage,
   CreatePost,
   Comments,
+  UserProfile,
 } from "./pages";
 
 const App = () => {
   return (
-    <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<OnBoarding />} />
-          <Route path="/authenticator" element={<Authenticator />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/homepage" element={<HomePage />} />
-          <Route path="/create-post" element={<CreatePost />} />
-          <Route path="/comments/:postId" element={<Comments />} />
-        </Routes>
-      </Router>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<OnBoarding />} />
+        <Route path="/authenticator" element={<Authenticator />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/homepage" element={<HomePage />} />
+        <Route path="/create-post" element={<CreatePost />} />
+        <Route path="/comments/:postId" element={<Comments />} />
+        <Route path="/profile/:userId" element={<UserProfile />} />
+      </Routes>
+    </Router>
   );
 };
 
