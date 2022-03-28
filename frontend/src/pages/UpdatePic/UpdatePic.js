ca653896190b0b90eec2891741601d336f74001a
Abhishek332 <itsabhiofficial.21@gmail.com>
2022-03-29 00:29:22 +0530

    Client implementation for update profile image complete

--- frontend/src/pages/UpdatePic/UpdatePic.js
+++ frontend/src/pages/UpdatePic/UpdatePic.js
@@ -1,7 +1,45 @@
-import React from "react";
+import { IMAGE_UPLOAD } from "../../redux/create_post/constant";
+import { useEffect } from "react";
+import { FileUploader } from "react-drag-drop-files";
+import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { imageUploader } from "../../redux/create_post/action";
+import { axios } from "../../api/axios";
+import "./UpdatePic.scss";
+
+const fileTypes = ["JPEG", "PNG", "WEBP", "JPG"];
 
 const UpdatePic = () => {
-  return <div>UpdatePic</div>;
+  const navigate = useNavigate(),
+    dispatch = useDispatch(),
+    loggedUserId = JSON.parse(localStorage.getItem("userInfo") ?? "")._id,
+    { imageUrl } = useSelector((state) => state.imageUpload);
+
+  useEffect(() => {
+    if (!localStorage.getItem("userInfo")) navigate("/");
+  }, [navigate]);
+
+  useEffect(() => {
+    if (!imageUrl) return;
+    axios
+      .put("/update-pic", imageUrl)
+      .then(() => {
+        dispatch({ type: IMAGE_UPLOAD.NULL });
+        navigate(`/profile/${loggedUserId}`);
+      })
+      .catch((error) => console.log(error));
+  }, [dispatch, imageUrl, loggedUserId, navigate]);
+
+  return (
+    <div className="update-pic-container">
+      <FileUploader
+        handleChange={(file) => dispatch(imageUploader(file))}
+        onDrop={(file) => dispatch(imageUploader(file))}
+        name="photo"
+        types={fileTypes}
+      />
+    </div>
+  );
 };
 
 export default UpdatePic;
