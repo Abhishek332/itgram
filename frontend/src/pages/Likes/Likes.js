699433404557e1e2a0ee5c974a4dd83bb703d060
Abhishek332 <itsabhiofficial.21@gmail.com>
2022-03-30 23:28:46 +0530

    working on likes page

---
+++ frontend/src/pages/Likes/Likes.js
@@ -0,0 +1,35 @@
+import { CommonBox } from "../../components";
+import { useParams } from "react-router-dom";
+import { useState, useEffect, useCallback } from "react";
+import { axios } from "../../api/axios";
+import "./Likes.scss";
+import { NavBar } from "../../components";
+
+const Likes = () => {
+  const { postId } = useParams(),
+    [likes, setLikes] = useState();
+
+  const fetchLikes = useCallback(() => {
+    axios
+      .get(`/get-likes/${postId}`)
+      .then(({ data: { likes } }) => setLikes(likes))
+      .catch((error) => console.log(error));
+  }, [postId]);
+
+  useEffect(() => {
+    fetchLikes();
+  }, [fetchLikes]);
+
+  return (
+    <>
+      <NavBar />
+      <div className="likes-page-container">
+        {likes?.map((e, i) => (
+          <CommonBox {...e} key={`likes-${i + 1}`} fetchData={fetchLikes} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default Likes;
