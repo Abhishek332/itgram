0819af43d18f076fdc2ea19b00a51839a41aa197
Abhishek332 <itsabhiofficial.21@gmail.com>
2022-07-26 16:55:35 +0530

    Added remote url for backend

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,16 +2,20 @@ import Axios from "axios";
 
 const userInfo = localStorage.getItem("userInfo") || null;
 
-let Header = {
-  "Content-Type": "application/json",
-};
+let headers = {
+    "Content-Type": "application/json",
+  },
+  baseURL = "http://localhost:5000/";
+
 if (userInfo) {
-  Header = {
+  headers = {
     "Content-Type": "application/json",
     Authorization: JSON.parse(userInfo).token,
   };
 }
 
-export const axios = Axios.create({
-  headers: Header,
-});
+if (process.env.NODE_ENV === "production") {
+  baseURL = "https://itgram-backend.herokuapp.com";
+}
+
+export const axios = Axios.create({ headers, baseURL });
