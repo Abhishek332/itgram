657cdb42f3ab8c4eccfb4c5561d0c1016bc0269b
Abhishek332 <itsabhiofficial.21@gmail.com>
2022-07-26 16:50:01 +0530

    Resolve cors issue and add remote url for backend production

--- backend/app.js
+++ backend/app.js
@@ -1,24 +1,25 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import Keys from './config/keys.js';
-import './models/user.js';
-import './models/post.js';
-import './models/portfolio.js';
-import { AuthRouter } from './routes/auth.js';
-import { PostRouter } from './routes/post.js';
-import { UserRouter } from './routes/user.js';
-import { PortfolioRouter } from './routes/portfolio.js';
-import path from 'path';
+import express from "express";
+import mongoose from "mongoose";
+import Keys from "./config/keys.js";
+import "./models/user.js";
+import "./models/post.js";
+import "./models/portfolio.js";
+import { AuthRouter } from "./routes/auth.js";
+import { PostRouter } from "./routes/post.js";
+import { UserRouter } from "./routes/user.js";
+import { PortfolioRouter } from "./routes/portfolio.js";
+// import path from 'path';
+import cors from "cors";
 
 const { MONGOURI } = Keys();
 
 //Connect with database
 mongoose.connect(MONGOURI);
-mongoose.connection.on('connected', () => {
-	console.log('Connect to Mongo');
+mongoose.connection.on("connected", () => {
+  console.log("Connect to Mongo");
 });
-mongoose.connection.on('error', (err) => {
-	console.log(`Connecting Error : ${err}`);
+mongoose.connection.on("error", (err) => {
+  console.log(`Connecting Error : ${err}`);
 });
 
 //create a app and assign port for listening
@@ -33,6 +34,15 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server is running at ${PORT}`));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "https://itgram.netlify.app",
+      "https://google.com",
+    ],
+  })
+);
 app.use(express.json());
 app.use(AuthRouter);
 app.use(PostRouter);
